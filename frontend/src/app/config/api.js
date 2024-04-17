078cd08a9f4767642700c0583c94f1498758e782
TuanAnh2401 <lta24012k2@gmail.com>
2024-04-17 22:47:54 +0700

    Remove FE

--- frontend/src/app/config/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-// nơi tương tác với API
-export const callFetchCategory = () => {
-    return axios.get('https://jsonplaceholder.typicode.com/users')
-}
--- frontend/src/app/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers } from "redux";
-import { cartReducer } from "./cartReducer";
-
-export default combineReducers({
-    cart: cartReducer
-}); 
-
